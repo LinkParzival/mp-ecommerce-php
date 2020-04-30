14b3b786cc67bc0476c14a75a419065ebd354e87
Angel Trejo (LinK) <angel.trejo@pymeweb.mx>
2020-04-30 01:51:29 -0500

    prrocesar datos

--- detail.php
+++ detail.php
@@ -187,7 +187,7 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <form action="" method="POST">
+                                    <form action="/procesar-pago.php" method="POST">
                                     <script
                                        src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
                                        data-preference-id="<?php echo $preference->id; ?>" data-button-label="Pagar la compra" data-elements-color="#2D3277">
---
+++ procesar-pago.php
@@ -0,0 +1,6 @@
+<?php
+echo "POST:";
+echo "<pre>"; var_dump($_POST); echo "</pre>";
+echo "GET:";
+echo "<pre>"; var_dump($_GET); echo "</pre>";
+?>
